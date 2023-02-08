bf4974cbd25958c7de854dcbdee3c142e7dcd534
Guilherme Leite <gleite13@hotmail.com>
2023-02-08 19:28:11 -0300

    56

---
+++ Mundo 2/56 - Analisador Completo.py
@@ -0,0 +1,5 @@
+for c in range(1, 5):
+    print('-=-'*3)
+    nome = str(input('Nome da {}º pessoa: ' .format(c)))
+    sexo = str(input('Sexo da {}º pessoa: ' .format(c)))
+    idade = int(input('Ano de nascimento da {}º pessoa: ' .format(c)))
