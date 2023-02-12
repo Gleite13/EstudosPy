8b9df4e04122776eedc0b5e38e2832f7fbf35cf5
Guilherme Leite <gleite13@hotmail.com>
2023-02-12 19:05:22 -0300

    ex 58

---
+++ Mundo 2/58 - Jogo da adivinhação 2.0.py
@@ -0,0 +1,10 @@
+import random
+n = random.randint(1, 10)
+x = int(input('Digite um numero de 0 a 10 para começar o JOGO: '))
+while x != n:
+    print('Maquina {} '.format(n))
+    print('Você {}' .format(x))
+    n = random.randint(1, 11)
+    x = int(input('Você errou, Tente outro numero: '))
+print('Maquina {} '.format(n))
+print('Você {}' .format(x))
