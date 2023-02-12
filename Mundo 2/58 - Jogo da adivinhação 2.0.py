90cd1ea094dc08e4283419e38b2565dd82603972
Guilherme Leite <gleite13@hotmail.com>
2023-02-12 19:10:35 -0300

    atualização de exercicio

--- Mundo 2/58 - Jogo da adivinhação 2.0.py
+++ Mundo 2/58 - Jogo da adivinhação 2.0.py
@@ -4,7 +4,7 @@ x = int(input('Digite um numero de 0 a 10 para começar o JOGO: '))
 while x != n:
     print('Maquina {} '.format(n))
     print('Você {}' .format(x))
-    n = random.randint(1, 11)
+    n = random.randint(1, 10)
     x = int(input('Você errou, Tente outro numero: '))
 print('Maquina {} '.format(n))
 print('Você {}' .format(x))
