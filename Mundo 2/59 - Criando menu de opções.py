53f865bdad098734da8d81934a85afa104a220c8
Guilherme Leite <gleite13@hotmail.com>
2023-02-16 13:32:26 -0300

    59 - menu

--- Mundo 2/59 - Criando menu de opções.py
+++ Mundo 2/59 - Criando menu de opções.py
@@ -1,15 +1,16 @@
+# Criando Menu de opções com exercicio utilizando While
+
 print('''Digite dois números''')
-n1 = float(input('Diga o primeiro número: '))
-n2 = float(input('Diga o segundo número: '))
-print('Menu de Opções: ')
-print('''[1] Somar
+x = 0
+while x != 5:
+    n1 = float(input('Diga o primeiro número: '))
+    n2 = float(input('Diga o segundo número: '))
+    print('Menu de Opções: ')
+    print('''[1] Somar
 [2] Multiplicar
 [3] Maior
 [4] Novos Números 
 [5] Sair do Programa''')
-
-x = 0
-while x != 5:
     x = int(input('Digite uma opção: '))
     if x == 1:
         print('A soma dos valores é: {:.2f}' .format(n1+n2))
@@ -18,7 +19,13 @@ while x != 5:
     if x == 3:
         if n1 > n2:
             print('O maior número é {}' .format(n1))
-        else:
+        if n2 > n1:
             print('O maior número é: {}' .format(n2))
+        else:
+            print('Números iguais.')
+    if x == 4:
+        print('Retornando ao menu...')
+    if x == 5:
+        print('Saindo do programa...')
 
 
