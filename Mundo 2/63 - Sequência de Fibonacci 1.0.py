75f37f2a066a2034774ff15164903a239e241b1d
Guilherme Leite <gleite13@hotmail.com>
2023-03-01 19:37:52 -0300

    ex 63 nao finalizado

---
+++ Mundo 2/63 - Sequência de Fibonacci 1.0.py
@@ -0,0 +1,20 @@
+#Sequência de Fibonacci WHILE
+
+print('Sequencia de Fibonacci: ')
+n = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+pausa = 1
+print('{} -> {} ' .format(t1, t2), end=' ')
+cont = 3
+#     while pausa != 0:
+    while cont <= n:
+        t3 = t1 + t2
+        print(' -> {}' .format(t3), end=' ')
+        t1 = t2
+        t2 = t3
+        cont += 1 #faz com que o contador pare no valor de n
+#    print('')
+#    seq = t3
+ #   pausa = int(input('Digite [1] para continuar os termos e [0] para sair: '))
+ #   n = int(input('Quantos termos deseja adicionar: '))
