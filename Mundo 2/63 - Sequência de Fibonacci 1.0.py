9c9a0448f528776be0d6a6f920166396cfc23434
Guilherme Leite <gleite13@hotmail.com>
2023-03-02 13:15:14 -0300

    ex 63

--- Mundo 2/63 - Sequência de Fibonacci 1.0.py
+++ Mundo 2/63 - Sequência de Fibonacci 1.0.py
@@ -6,14 +6,15 @@ t1 = 0
 t2 = 1
 pausa = 1
 print('{} -> {} ' .format(t1, t2), end=' ')
-cont = 3
+cont = 3 # 3 pois  t1, t2 e t3 ja valem na contagem
 #     while pausa != 0:
-    while cont <= n:
-        t3 = t1 + t2
-        print(' -> {}' .format(t3), end=' ')
-        t1 = t2
-        t2 = t3
-        cont += 1 #faz com que o contador pare no valor de n
+while cont <= n:
+    t3 = t1 + t2
+    print(' -> {}'.format(t3), end=' ')
+    t1 = t2
+    t2 = t3
+    cont += 1  # faz com que o contador pare no valor de n
+print(' -> Fim')
 #    print('')
 #    seq = t3
  #   pausa = int(input('Digite [1] para continuar os termos e [0] para sair: '))
