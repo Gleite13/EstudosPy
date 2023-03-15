56bfcd3a23e343f96ddd31265e75ce327ebe4073
Guilherme Leite <gleite13@hotmail.com>
2023-03-15 19:49:34 -0300

    70

--- Mundo 2/70 - Estatísticas em produtos.py
+++ Mundo 2/70 - Estatísticas em produtos.py
@@ -5,17 +5,18 @@
 
 #C) qual é o nome do produto mais barato.
 
-total = tot10 = 0
+total = tot10 = cont = menor =  0
 barato = ' '
 while True:
     nome = str(input('Qual o nome do produto: '))
     valor = float(input('Qual o valor do produto '))
     total += valor
+    cont += 1
     if valor > 1000:
         tot10 += 1
-    if valor < 
-
-
+    if cont == 1 or valor < menor: # faz com que o menor produto seja adiocionado as variasveis
+        menor = valor
+        barato = nome
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
@@ -23,3 +24,4 @@ while True:
         break
 print(f'O total das compras é de {total:.2f}R$')
 print(f'Foram visualizada(s) {tot10} compras acima de mil reais')
+print(f'O Produto mais barato foi {barato} que custa R${menor:.2f}')
