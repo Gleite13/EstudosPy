88a3ddf0c97f2cb37cd91c7dc5b9a5939cc9ca0c
Guilherme Leite <gleite13@hotmail.com>
2023-03-23 19:29:53 -0300

    73

--- Mundo 3/73 - Tuplas com Time de futebol.py
+++ Mundo 3/73 - Tuplas com Time de futebol.py
@@ -13,16 +13,29 @@ times = ('Flamengo', 'Palmeiras', 'Athletico Paranaense', 'Atlético Mineiro',
          'Ceará', 'Bahia', 'Botafogo', 'Red Bull Bragantino', 'Cruzeiro',
          'Goiás', 'Cuiabá')
 while True:
-    print('-=-' * 30)
-    print('Painel do Brasileirão')
-    print('-=-' * 30)
-    print('Os primeiros times 5 times do Campeonato Brasileiro São:')
-    print(times[0:5])
-    print('Os 4 Ultimos do Campeonato Brasileiro São:')
-    print(times[16:])
-    print('Times em Ordem Alfabética:')
-    print(sorted(times))
-    print(f'O Grêmio esta na {times.index("Grêmio")+1}° Posição')
+    print('-=-' * 15)
+    print('Painel do Brasileirão'.center(40))
+    print('-=-' * 15)
+    print('[1]Os 5 primeiros Colocados\n'
+          '[2]Os 4 ultimos colocados\n'
+          '[3]Times em ordem alfabética\n'
+          '[4]Em que posição o Grêmio esta?\n'
+          '[5]Para sair')
+    n = int(input('Qual opção desejada?'))
+    if n == 1:
+        print('Os primeiros times 5 times do Campeonato Brasileiro São:')
+        print(times[0:5])
+    if n == 2:
+        print('Os 4 Ultimos do Campeonato Brasileiro São:')
+        print(times[16:])
+    if n == 3:
+        print('Times em Ordem Alfabética:')
+        print(sorted(times))
+    if n == 4:
+        print(f'O Grêmio esta na {times.index("Grêmio")+1}° Posição')
+    if n == 5:
+        print('Obrigado por participar')
+        break
 
 
 
