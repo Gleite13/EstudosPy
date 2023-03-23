6b4e8d1a4063aaad50dc2825d980493d1adcfb91
Guilherme Leite <gleite13@hotmail.com>
2023-03-23 19:45:29 -0300

    75 começando

---
+++ Mundo 3/74 - Maior e menor valores em Tupla.py
@@ -0,0 +1,9 @@
+# Criar um programa que leia 5 números aleátorios e colocar em uma tupla
+# Mostrar a listagem dos numeros gerados
+# Indicar maior e menor da lista
+
+from random import randint
+n = 5
+tupla = ()
+for c in range(0, 11):
+
