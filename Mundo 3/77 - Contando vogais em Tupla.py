1dd9271117bcf56f5fd3994184ce0dd6c1d4fa64
Guilherme Leite <gleite13@hotmail.com>
2023-06-06 15:20:58 -0300

    76, 77 iniciando

---
+++ Mundo 3/77 - Contando vogais em Tupla.py
@@ -0,0 +1,8 @@
+#Crie um programa que tenha uma tupla com várias palavras
+# (não usar acentos). Depois disso, você deve mostrar,
+# para cada palavra, quais são as suas vogais.
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO',
+            'GRATIS', 'ESTUDAR', 'PRATICAR', 'MERCADO', 'PROGRMADOR', 'FUTURO')
+for vogais in palavras:
+    print(vogais)
+    
