6a3da9dad1b2ba88fe0dd1059979389f9661bcce
Guilherme Leite <gleite13@hotmail.com>
2023-06-08 15:36:32 -0300

    78 +

--- Mundo 3/78 - Maior e Menor valores na Lista.py
+++ Mundo 3/78 - Maior e Menor valores na Lista.py
@@ -20,3 +20,21 @@ if menor in numeros:
     print(f'O menor número é {menor} e esta na posição: {numeros.index(menor)}')
 
 
+##############
+
+listanum = []
+mai = 0
+men = 0
+for n in range(0, 5):
+    listanum.append(int(input('Digite um numero: ')))
+    if n == 0:
+        mai = men = listanum[n]
+    else:
+        if listanum[n] > mai:
+            mai = listanum[n]
+        if listanum[n] > men:
+            men = listanum[n]
+print(f'Você digitou os valores {listanum}')
+print(f'O maior número foi {mai} ')
+print(f'O menor número foi {men}')
+
