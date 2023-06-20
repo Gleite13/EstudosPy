b9813bc35805becd130eee260191f971496f84d6
Guilherme Leite <gleite13@hotmail.com>
2023-06-20 09:11:19 -0300

    82

--- Mundo 3/82 - Dividindo valores em várias listas.py
+++ Mundo 3/82 - Dividindo valores em várias listas.py
@@ -21,3 +21,20 @@ while True:
 print(lista)
 print(par)
 print(impar)
+
+
+###############################
+
+num = []
+pares = []
+impares = []
+while True:
+    num.append(int(input('Digite um número: ')))
+    res = str(input('Deseja continuar? [S/N]: '))
+    while res in 'Nn':
+        break
+for i, v in enumerate(num): #indice e valor
+    if v % 2 == 0:
+        pares.append(v)
+    else:
+        impares.append(v)
