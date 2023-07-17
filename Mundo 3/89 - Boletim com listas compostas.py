19fe180b5073688dfaceec9fe0b8af2b0923baf1
Guilherme Leite <gleite13@hotmail.com>
2023-07-17 19:14:57 -0300

    89

--- Mundo 3/89 - Boletim com listas compostas.py
+++ Mundo 3/89 - Boletim com listas compostas.py
@@ -2,27 +2,27 @@
 # mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas
 # de cada aluno individualmente.
 
-# bagaça ainda nao saiu mas vai!!!!
-
-
-alunos = []
-temp = []
-temp2 = []
-todos = 999
+ficha = []
 while True:
     nome = str(input('Nome: '))
-    temp.append(nome)
-    for c in range (1, 3):
-        nota = int(input(f'Nota {c}: '))
-        temp2.append(nota)
-    alunos.append(temp[:])
-    alunos.append(temp2[:])
-    temp.clear()
-    temp2.clear()
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input(('Nota 2: ')))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
     resp = str(input('Deseja continuar? [S/N]: '))
     if resp in 'Nn':
         break
-for l in range(0, len(alunos)):
-    for c in range(0, len(alunos)):
-        print(alunos[l])
-print(alunos)
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":> 8.1f}')
+print('-=' * 30)
+for i, a in enumerate(ficha)
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8}')
+while True:
+    print('-' * 35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('Finalizando...')
+        break
+    if opc <= len(ficha) -1
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<<< VOLTE SEMPRE >>>>')
