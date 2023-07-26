970f37504e804e26b7d20308c5c97da56c8795f9
Guilherme Leite <gleite13@hotmail.com>
2023-07-26 19:37:34 -0300

    94 tentativa

---
+++ Mundo 3/94 - Unindo dicionários e listas.py
@@ -0,0 +1,27 @@
+# Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário
+# e todos os dicionários em uma lista. No final, mostre: A) Quantas pessoas foram cadastradas B)
+# A média de idade C) Uma lista com as mulheres D) Uma lista de pessoas com idade acima da média
+
+dicionario = {}
+lista = []
+temp = []
+while True:
+    dicionario['nome'] = str(input('Nome: '))
+    while True:
+        dicionario['sexo'] = str(input('Sexo [M/F]: '))
+        if dicionario['sexo'] in 'MmFf':
+            break
+        else:
+            print('ERRO! Por favor, digite apenas M ou F')
+    dicionario['idade'] = int(input('Idade: '))
+    temp.append(dicionario.items()[:])
+    lista.append(temp[:])
+    temp.clear()
+
+    resp = str(input('Deseja Continuar? [S/N]: '))
+    if resp in 'Nn':
+        break
+    elif resp not in 'Ss':
+        print('ERRO! Por favor, digite M ou F.')
+print(dicionario)
+print(lista)
