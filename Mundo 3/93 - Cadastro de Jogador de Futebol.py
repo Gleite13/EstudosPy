3ac4fd0c8859cd01b5afedb0a1fcdbc89b0b74db
Guilherme Leite <gleite13@hotmail.com>
2023-08-21 20:43:04 -0300

    95 continuar

--- Mundo 3/93 - Cadastro de Jogador de Futebol.py
+++ Mundo 3/93 - Cadastro de Jogador de Futebol.py
@@ -3,28 +3,28 @@
 # Depois vai ler a quantidade de gols feitos em cada partida. No final,
 # tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 
-aproveitamento = {}
-gol = []
-total = tot = 0
-aproveitamento['nome'] = str(input('Nome do Jogador: '))
-part = int(input(f'Quantas partidas {aproveitamento["nome"]} jogou? '))
-for c in range(0, part):
-    gols = int(input(f'   Quantos gols na partida {c}: '))
-    gol.append(gols)
-    total += gols
-    tot += 1
-aproveitamento['gols'] = gol
-aproveitamento['total'] = total
-print('-=' * 30)
-print(aproveitamento)
-print('-=' * 30)
-for k, v in aproveitamento.items():
-    print(f'O campo {k} tem o valot {v}')
-print('-=' * 30)
-print(f'O jogador {aproveitamento["nome"]} jogou {tot}')
-for c, l in enumerate(gol):
-    print(f'    Na partida {c}, fez {l} gols.')
-print(f'Foi um total de {tot}')
+#aproveitamento = {}
+#gol = []
+#total = tot = 0
+#aproveitamento['nome'] = str(input('Nome do Jogador: '))
+#part = int(input(f'Quantas partidas {aproveitamento["nome"]} jogou? '))
+#for c in range(0, part):
+#    gols = int(input(f'   Quantos gols na partida {c}: '))
+#    gol.append(gols)
+#    total += gols
+#    tot += 1
+#aproveitamento['gols'] = gol
+#aproveitamento['total'] = total
+#print('-=' * 30)
+#print(aproveitamento)
+#print('-=' * 30)
+#for k, v in aproveitamento.items():
+#    print(f'O campo {k} tem o valot {v}')
+#print('-=' * 30)
+#print(f'O jogador {aproveitamento["nome"]} jogou {tot}')
+#for c, l in enumerate(gol):
+#    print(f'    Na partida {c}, fez {l} gols.')
+#print(f'Foi um total de {tot}')
 
 ########################################
 
@@ -33,7 +33,7 @@ partidas = list()
 jogador['nome'] = str(input('Nome do Jogador: '))
 tota = int(input(f'Quantas partidas {jogador["nome"]} jogou?'))
 for c in range(0, tota):
-    partidas.append(int(input(f'    Quantos gols na partida {c}? '))
+    partidas.append(int(input(f'    Quantos gols na partida {c}? ')))
 jogador['gols'] = partidas[:]
 jogador['total'] = sum(partidas)
 print('-=' * 30)
--- Mundo 3/95 - Aprimorando os Dicionários.py
+++ Mundo 3/95 - Aprimorando os Dicionários.py
@@ -3,19 +3,34 @@
 
 jogador = dict()
 partidas = list()
-jogador['nome'] = str(input('Nome do Jogador: '))
-tota = int(input(f'Quantas partidas {jogador["nome"]} jogou?'))
-for c in range(0, tota):
-    partidas.append(int(input(f'    Quantos gols na partida {c}? '))
-jogador['gols'] = partidas[:]
-jogador['total'] = sum(partidas)
+lista = []
+while True:
+    jogador['nome'] = str(input('Nome do Jogador: '))
+    tota = int(input(f'Quantas partidas {jogador["nome"]} jogou?'))
+    for c in range(0, tota):
+        partidas.append(int(input(f'    Quantos gols na partida {c}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    lista.append(jogador.copy())
+    jogador.clear()
+    partidas.clear()
+    resp = str(input('Deseja continuar? [S/N]:'))
+    if resp in 'Nn':
+        break
 print('-=' * 30)
+print('COD    NOME    GOLS          TOTAL')
+print('-' * 30)
+for k, v in enumerate(lista):
+    print(f'{k}    {v["nome"]}    {v["gols"]}          {v["total"]}')
+
+print(partidas)
 print(jogador)
-print('-=' * 30)
-for k, v in jogador.items():
-    print(f'O campo {k} tem o valor {v}')
-print('-=' * 30)
-print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
-for i, v in enumerate(jogador['gols']):
-    print(f'   =>Na partida {i}, fez {v} gols.')
-print(f'Foi um total de {jogador["total"]} gols')
+print(lista)
+#print('-=' * 30)
+#print(jogador)
+#print('-=' * 30)
+#for k, v in jogador.items():
+#    print(f'O campo {k} tem o valor {v}')
+#print('-=' * 30)
+#print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+#print(f'Foi um total de {jogador["total"]} gols')
