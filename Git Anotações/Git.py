d0ef3a18c8f9d443501378d156cf5e6c24add6c9
Guilherme Leite <gleite13@hotmail.com>
2024-04-30 10:57:32 -0300

    PASTA GIT

---
+++ Git Anotações/Git.py
@@ -0,0 +1,7 @@
+git add . # para verificar arquivos a serem adicionados
+
+git status # para saber sobre os status
+
+git commit -m "comentario" # fazer commit com comentario
+
+git push # para jogar arquivos do commit
