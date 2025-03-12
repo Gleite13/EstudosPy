f0b5df1126b837f5b0a774df2b493bc6258d6e7b
Guilherme Leite <gleite13@hotmail.com>
2025-03-11 23:08:02 -0300

    TEST
    TEST

--- Mundo 1/Calcular media.py
+++ Mundo 1/Calcular media.py
@@ -1,4 +1,4 @@
 n1 = float(input('Digite o valor da primera nota: '))
 n2 = float(input('Digite o valor da segunda nota: '))
-m = (n1+n1)/2
+m = (n1+n2)/2
 print('A media entre {} e {} é: {}'.format(n1, n2, m))
