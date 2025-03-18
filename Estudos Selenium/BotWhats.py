31069503d85db4e29fb39110962b5789f8b6a60e
Guilherme Leite <gleite13@hotmail.com>
2025-03-18 00:20:19 -0300

    botwhats

--- Estudos Selenium/BotWhats.py
+++ Estudos Selenium/BotWhats.py
@@ -1,18 +1,51 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import webbrowser
+import openpyxl
+import pyautogui
+from urllib.parse import quote  # para enviar corretamente a mensagem  dentro do link
 from time import sleep
 
-navegador = webdriver.Chrome()
+# navegador = webdriver.Chrome()
 
-navegador.get('https://web.whatsapp.com/')
+# navegador.get('https://web.whatsapp.com/')
 
-while True:
-    while len(navegador.find_elements(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/canvas')):
-        pass
+# while True:
+# while len(navegador.find_elements(By.XPATH,
+#  '//*[@id="app"]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/canvas')):
+# pass
 
-    while len(navegador.find_elements(By.ID, 'side')) < 1:
-        pass
-    
-    #grupo = navegador.find_element(By.ID,'//*[@id="pane-side"]/div[1]/div/div/div[4]/div/div/div/div[2]')
-    #grupo.click()
+#   while len(navegador.find_elements(By.ID, 'side')) < 1:
+#        pass
+
+#    break
+
+webbrowser.open('https://web.whatsapp.com/')
+sleep(30)
+
+workbook = openpyxl.load_workbook('clientes.xlsx')
+paginaClientes = workbook['Planilha1']
+
+for linha in paginaClientes.iter_rows(min_row=2):
+    nome = linha[0].value
+    telefone = linha[1].value
+    vencimento = linha[2].value
+
+    mensagem = f'Olá {nome} seu boleto vence {vencimento}'
+
+    try:
+        linkMensagemFormatada = f'https://web.whatsapp.com/send?phone={telefone}&text="{quote(mensagem)}"'
+        webbrowser.open(linkMensagemFormatada)
+        sleep(10)
+
+        seta = pyautogui.locateOnScreen('seta.png')
+        
+        sleep(5)
+
+        pyautogui.click(seta[0], seta[1])
+
+        sleep(5)
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(5)
+    except:
+        print(f'Não foi possivel mandar a mensagem')
+        with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
+            arquivo.write(f'{nome},{telefone}')
--- Mundo 1/Calcular media.py
+++
@@ -1,4 +0,0 @@
-n1 = float(input('Digite o valor da primera nota: '))
-n2 = float(input('Digite o valor da segunda nota: '))
-m = (n1+n2)/2
-print('A media entre {} e {} é: {}'.format(n1, n2, m))
